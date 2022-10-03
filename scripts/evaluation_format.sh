7ff822d08f2f6f9ae09ad69e85f3133b6a886722
hoonst <yoonhs1993@nate.com>
2022-10-03 06:09:33 +0000

    temp save

--- scripts/evaluation_format.sh
+++ scripts/evaluation_format.sh
@@ -3,7 +3,7 @@ qv_link="false"
 dist_relation="true"
 orthog="false"
 orth_init="false"
-bi_way="true"
+bi_way="false"
 bi_match="true"
 
 exp_name="bs=$bs,qv_link=$qv_link,dist=$dist_relation,orthog=$orthog,orth_init=$orth_init,bi_way=$bi_way,bi_match=$bi_match"
